13baa3f385ed33827b6b3a64b3b14b4e5f3cdb96
Nacho <nachochambo@gmail.com>
2020-11-04 11:25:13 +0100

    Splash + Login + PanelCliente
    Se ha realizado en diseño e implementación de estas 3 partes de la app

---
+++ codigoFuente/AplicacionMovil/BTLE/app/src/main/java/com/example/daferfus_upv/btle/SplashScreen.java
@@ -0,0 +1,57 @@
+package com.example.daferfus_upv.btle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    //Variables animaciones
+    Animation animArriba, animAbajo;
+    ImageView imagen;
+    TextView eslogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_screen);
+
+        //Animaciones
+        animArriba = AnimationUtils.loadAnimation(this, R.anim.arriba_splash_anim);
+        animAbajo = AnimationUtils.loadAnimation(this, R.anim.abajo_splash_anim);
+
+        //Hooks
+        imagen = findViewById(R.id.imagenSplash);
+        eslogan = findViewById(R.id.textViewSplash);
+
+        imagen.setAnimation(animArriba);
+        eslogan.setAnimation(animAbajo);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+
+        thread.start();
+    }
+
+}
